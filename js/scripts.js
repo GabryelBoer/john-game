7a08ed05296ef9952f3e08a21eb27615f0b4e5f3
GabryelBoer <107651799+GabryelBoer@users.noreply.github.com>
2023-01-30 12:07:09 -0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ const john = document.querySelector('.john');
 const cacto = document.querySelector('#cacto');
 const score = document.querySelector('.score')
 let count = 0;
+let gameover = false
 let alreadyJump = false;
 
 const jump = () => {
@@ -29,13 +30,17 @@ const loop = setInterval(() => {
         john.style.bottom = `${johnPosition}px`;
         john.src = '/images/johndeath.png';
         john.style.marginLeft = '-15px';
-
+        gameover = true;
         clearInterval(loop);
+        setTimeout(() => {
+            alert("Game Over")
+            location.reload();
+        }, 100);
     }
-
     count++;
     score.innerHTML = `SCORE: ${count}`;
 }, 10);
+
 
 addEventListener("keydown", (e) => {
     if(e.code === 'Space' || e.code === 'ArrowUp' || e.key.toLowerCase() === 'w') {
